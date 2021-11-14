b9325a9cf90e030e98b60aafbb9ce71f775be272
Artem <jurjev.artem@gmail.com>
2021-11-14 19:40:29 +0300

    fix hover plus image

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -2,21 +2,28 @@ import React, { FC } from 'react'
 import logo from "../assets/images/logo.png";
 import CartList from '../components/CartList';
 import Logo from '../components/Logo';
+import emptyCartImage from '../assets/images/empty-cart.jpg'
 import { useTypedSelector } from '../hooks/useTypedSelector';
 
 const CartPage: FC = () => {
-    const {totalPrice} = useTypedSelector(state => state.cart);
+    const { lines, totalPrice } = useTypedSelector(state => state.cart);
     return (<>
         <div className="header cart">
-            <Logo/>
+            <Logo />
         </div>
         <div className="cart-title">Shopping Cart</div>
         <div className="cart-body">
-            <CartList/>
-            <div className="cart-body-total">Subtotal: USD {totalPrice}</div>
+            {lines.length > 0 && (<>
+                <CartList />
+                <div className="cart-body-total">
+                    Subtotal: USD {totalPrice}
+                </div>
+            </>
+            )}
+            {lines.length === 0 && (<img className="cart-body-empty" src={emptyCartImage} alt={emptyCartImage} />)}
         </div>
-        <button className="cart-checkout_button">Checkout</button>
-        </>
+        {lines.length > 0 && (<button className="cart-checkout_button">Checkout</button>)}
+    </>
     )
 }
 
